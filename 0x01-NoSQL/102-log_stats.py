c826395e6a3e60eacc5fd30cc31a44c9951ba3b5
husham35 <husham35@gmail.com>
2024-08-13 19:35:19 +0000

    task 102 fixes

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -11,23 +11,23 @@ def log_stats():
     """
     Prints a collection of nginx database log:
     """
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    logs_collection = client.logs.nginx
-    total_logs = logs_collection.count_documents({})
-    get_logs = logs_collection.count_documents({"method": "GET"})
-    post_logs = logs_collection.count_documents({"method": "POST"})
-    put_logs = logs_collection.count_documents({"method": "PUT"})
-    patch_logs = logs_collection.count_documents({"method": "PATCH"})
-    delete_logs = logs_collection.count_documents({"method": "DELETE"})
+    client_db = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client_db.logs.nginx
+    total = logs_collection.count_documents({})
+    get = logs_collection.count_documents({"method": "GET"})
+    post = logs_collection.count_documents({"method": "POST"})
+    put = logs_collection.count_documents({"method": "PUT"})
+    patch = logs_collection.count_documents({"method": "PATCH"})
+    delete = logs_collection.count_documents({"method": "DELETE"})
     path = logs_collection.count_documents(
         {"method": "GET", "path": "/status"})
-    print(f"{total_logs} logs")
+    print(f"{total} logs")
     print("Methods:")
-    print(f"\tmethod GET: {get_logs}")
-    print(f"\tmethod POST: {post_logs}")
-    print(f"\tmethod PUT: {put_logs}")
-    print(f"\tmethod PATCH: {patch_logs}")
-    print(f"\tmethod DELETE: {delete_logs}")
+    print(f"\tmethod GET: {get}")
+    print(f"\tmethod POST: {post}")
+    print(f"\tmethod PUT: {put}")
+    print(f"\tmethod PATCH: {patch}")
+    print(f"\tmethod DELETE: {delete}")
     print(f"{path} status check")
     print("IPs:")
     sorted_ips = logs_collection.aggregate(
@@ -37,7 +37,7 @@ def log_stats():
     for s in sorted_ips:
         if i == 10:
             break
-        print(f"\t{s.get_logs('_id')}: {s.get_logs('count')}")
+        print(f"\t{s.get('_id')}: {s.get('count')}")
         i += 1
 
 
