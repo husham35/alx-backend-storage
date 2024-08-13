b1c014f549e6e099e1b217474d6377bb83875091
husham35 <husham35@gmail.com>
2024-08-13 19:24:18 +0000

    first push

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Function that inserts a new document in a collection based on kwargs
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    mongo_collection will be the pymongo collection object
+    Returns the new `_id`
+    """
+    new_doc = mongo_collection.insert_one(kwargs)
+    return new_doc.inserted_id
