9cfbcfe7d3aa28b4d39e3c478d526cb1e698688f
husham35 <husham35@gmail.com>
2024-08-14 19:40:45 +0000

    task web.py more fixes

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -22,30 +22,30 @@ def data_cacher(method: Callable) -> Callable:
         """
         A wrapper function for caching the output.
         """
-        # redis_store.incr(f'count:{url}')
-        # result = redis_store.get(f'result:{url}')
-        
-        # if result:
-        #     return result.decode('utf-8')
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
 
-        # result = method(url)
-        # redis_store.set(f'count:{url}', 0)
-        # redis_store.setex(f'result:{url}', 10, result)
-        # return result
-        
-        key = "count:{}".format(url)
-        value = "cached:{}".format(url)
+        if result:
+            return result.decode('utf-8')
 
-        redis_store.incr(key)
-        cache = redis_store.get(value)
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
 
-        if cache:
-            return cache.decode('utf-8')
+        # key = "count:{}".format(url)
+        # value = "cached:{}".format(url)
 
-        html = method(url)
-        redis_store.set(key, 0)
-        redis_store.setex(value, 10, html)
-        return html
+        # redis_store.incr(key)
+        # cache = redis_store.get(value)
+
+        # if cache:
+        #     return cache.decode('utf-8')
+
+        # html = method(url)
+        # redis_store.set(key, 0)
+        # redis_store.setex(value, 10, html)
+        # return html
 
     return invoker
 
