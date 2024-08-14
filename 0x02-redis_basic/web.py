02a8cc37dd640b000322e5029b39c74cbeeaaabc
husham35 <husham35@gmail.com>
2024-08-14 19:54:58 +0000

    another fixe for web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,55 +4,19 @@ A module that uses the requests module to obtain the HTML content of a
 particular URL and returns it.
 """
 
-from functools import wraps
 import redis
 import requests
-from typing import Callable
+from datetime import timedelta
 
 
-redis_store = redis.Redis()
-
-
-def data_cacher(method: Callable) -> Callable:
-    """
-    A wrapper function that caches the output of fetched data.
-    """
-    @wraps(method)
-    def invoker(url) -> str:
-        """
-        A wrapper function for caching the output.
-        """
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-
-        if result:
-            return result.decode('utf-8')
-
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-
-        # key = "count:{}".format(url)
-        # value = "cached:{}".format(url)
-
-        # redis_store.incr(key)
-        # cache = redis_store.get(value)
-
-        # if cache:
-        #     return cache.decode('utf-8')
-
-        # html = method(url)
-        # redis_store.set(key, 0)
-        # redis_store.setex(value, 10, html)
-        # return html
-
-    return invoker
-
-
-@data_cacher
 def get_page(url: str) -> str:
     """
     Returns the HTML content of a particular URL
     """
-    return requests.get(url).text
+    # return requests.get(url).text
+    r = redis.Redis()
+    key = "count:{}{}{}".format('{', url, '}')
+    r.incr(key)
+    res = requests.get(url)
+    r.setex(url, timedelta(seconds=10), res.text)
+    return res.text
